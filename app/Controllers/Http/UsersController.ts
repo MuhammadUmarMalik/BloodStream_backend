7b5e86068e9441b16718c26d74c2049da417bd14
MuhammadUmarMalik <malikgraphics811@gmail.com>
2023-08-21 23:21:30 +0500

    User and Donations and auth controller are done

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,60 +1,73 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Database from '@ioc:Adonis/Lucid/Database';
 import User from 'App/Models/User';
+import { Createvalidations,Updatevalidations } from 'App/Validators/UserValidator';
 
 export default class UserController {
   public async index({ response }: HttpContextContract) {
     try {
       const users = await User.all();
-      return response.ok(users);
+      return response.send(users);
     } catch (error) {
-      return response.internalServerError({ error: 'An error occurred while fetching users.' });
+      return response.send({ error: 'An error occurred while fetching users.' });
     }
   }
 
   public async show({ params, response }: HttpContextContract) {
     try {
-      const user = await User.findOrFail(params.id);
-      return response.ok(user);
+      // const user = await User.find(params.blood_group);
+      const user= await User.query().from('users').where('blood_group',params.blood_group)
+      console.log("user-======>",user)
+      return response.send(user);
     } catch (error) {
-      return response.notFound({ error: 'User not found.' });
+      console.log(error)
+      return response.send({error});
     }
   }
-
+// store the new user data in db
   public async store({ request, response }: HttpContextContract) {
     try {
+      await request.validate({ schema: Createvalidations });
       const userData = request.only([
         'name',
         'email',
-        'password',
         'blood_group',
         'phone_number',
-        'latitude',
-        'longitude',
+        'address',
+        'city',
+        'enable_request',
+        'donation_date',
+        'donation_count'
       ]);
-
+      console.log('user------->',request)
       const user = await User.create(userData);
       return response.created(user);
     } catch (error) {
-      return response.badRequest({ error: 'Failed to create user.' });
+      return response.send(error);
     }
   }
 
   public async update({ params, request, response }: HttpContextContract) {
     try {
+      await request.validate({ schema: Updatevalidations });
       const user = await User.findOrFail(params.id);
-      const userData = request.only([
+      const userData = request.only([ 
         'name',
+        'email',
+        'gender',
         'blood_group',
         'phone_number',
-        'latitude',
-        'longitude',
+        'address',
+        'city',
+        'enable_request',
+        'donation_date',
+        'donation_count'
       ]);
-
-      user.merge(userData);
-      await user.save();
-      return response.ok(user);
+      const updateData= user.merge(userData);
+      await updateData.save();
+      return response.send(updateData);
     } catch (error) {
-      return response.notFound({ error: 'User not found or update failed.' });
+      return response.send(error);
     }
   }
 
@@ -62,9 +75,9 @@ export default class UserController {
     try {
       const user = await User.findOrFail(params.id);
       await user.delete();
-      return response.noContent();
+      return response.send('user is deleted');
     } catch (error) {
-      return response.notFound({ error: 'User not found or delete failed.' });
+      return response.send({ error: 'User not found or delete failed.' });
     }
   }
 }
--- database/migrations/1692095433032_users.ts
+++ database/migrations/1692095433032_users.ts
@@ -7,21 +7,23 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('name',255)
+      table.string('gender',255)
       table.string('blood_group',255)
-      table.string('Phone_number',255)
-      table.decimal('latitude', 10, 7);
-      table.decimal('longitude', 10, 7);
+      table.string('phone_number',255)
+      table.string('address',255);
+      table.string('city',255);
       table.boolean('enable_request')
+      table.dateTime('donation_date')
       table.string('email', 255).notNullable().unique()
       table.string('provider');
       table.string('provider_id');
-      table.string('remember_me_token').nullable()
+      table.string('remember_me_token')
 
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true }).notNullable()
-      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
     })
   }
 
